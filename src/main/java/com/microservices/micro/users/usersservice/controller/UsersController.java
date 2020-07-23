9de3692ea8923422b0a6cee3c43b69459940987e
srpepperoni <jaimeyera@gmail.com>
2020-07-23 23:22:49 +0200

    Default Commit

---
+++ src/main/java/com/microservices/micro/users/usersservice/controller/UsersController.java
@@ -0,0 +1,22 @@
+package com.microservices.micro.users.usersservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsersController {
+	
+	private static final String template = "Hello, %s!";
+	
+	@GetMapping(value="/test")
+	public String test() {
+		return "Hello World from Users Service";
+	}
+	
+	@GetMapping("/greeting/{name}")
+    public String greeting2(@PathVariable("name") String name) {
+        return String.format(template, name) ;		
+    }
+
+}
